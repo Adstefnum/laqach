b0bdc0e7e75564e91296eccdb22a38fb8ddea091
Adstefnum <bauerstevenson@gmail.com>
2022-06-14 09:49:11 +0300

    working on receiving funds

--- contracts/FundmeContract.sol
+++ contracts/FundmeContract.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.7;
 
 
  error NotOwner();
+ error InsufficientFundAmount();
 
 contract FundMe{
    uint256 constant MinimumLocalCurrencyFundAmount = 50*10e18;
@@ -13,24 +14,32 @@ contract FundMe{
 
    
    mapping(address=>uint256) AddresstoFundedAmount;
+   address[] Funders;
    
-   
-
-    
     constructor(){
         owner = msg.sender;
     }
 
-    function ReceiveFunds() public payable{}
+    function ReceiveFunds() public payable{
+        AddresstoFundedAmount[msg.sender] += msg.value;
+        Funders.push(msg.sender);
+    }
     function WithdrawFunds() public OnlyOwner{}
+
     receive() external payable{
         ReceiveFunds();
     }
+
     fallback() external payable{
         ReceiveFunds();
     }
+
     modifier OnlyOwner{
         if(msg.sender!=owner) revert NotOwner();
         _;
+    }
+
+    modifier SufficientFundAmount{
+        if(msg.value!=MinimumCyrptoCurrencyFundAmount) revert InsufficientFundAmount();
     }
 }
