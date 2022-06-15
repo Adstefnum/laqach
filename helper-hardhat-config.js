ea468c9950c8c008cedc88bdcf0c609e57efb4cc
Adstefnum <bauerstevenson@gmail.com>
2022-06-16 02:27:51 +0300

    adding deployment config for various networks

---
+++ helper-hardhat-config.js
@@ -0,0 +1,27 @@
+NetworkConfig = {
+	1:{
+		name:"Ethereum Mainet",
+		EthUSDPriceFeedAddress:
+	},
+	4:{
+		name:"Rinkeby",
+		EthUSDPriceFeedAddress:
+	},
+	42:{
+		name:"Kovan",
+		EthUSDPriceFeedAddress:
+	},
+	1337:{
+		name:"Localhost",
+		EthUSDPriceFeedAddress:
+	},
+	
+	31337:{
+		name:"Hardhat",
+		EthUSDPriceFeedAddress:
+	},
+}
+
+module.exports = {
+	NetworkConfig,
+}
